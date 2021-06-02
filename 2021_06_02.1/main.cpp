ef30b610608f182e3e9e98c2c0e2ac1b24c643db
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-06-02 22:35:10 +0900

    Initial Commit

---
+++ 2021_06_02.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2021_06_02.1
+//
+//  Created by Hiroaki Ishiga on 2021/06/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
